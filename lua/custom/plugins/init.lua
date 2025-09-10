d5f08b63c9e8fd887dc319b1cfc63a2fc441d832
YanMinChan <yanmin.chan08@gmail.com>
2025-09-10 17:19:54 +0800

    fix virtual text

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -65,8 +65,8 @@ return {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "VeryLazy",
     priority = 1000,
-    opts = {},
     config = function()
+      require('tiny-inline-diagnostic').setup()
       vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
     end
   },
